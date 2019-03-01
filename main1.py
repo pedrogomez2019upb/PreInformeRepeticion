0b34a649b4f60980adf35e52c3a86ceb9a0cee5a
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 20:48:31 -0500

    Este archivo se utilizó mediante la función while para demostrar que para este primer ejemplo no se puede utilizar dicha función. Sino , este va a hacer un bucle sin fin

--- main1.py
+++ main1.py
@@ -1,1 +1,9 @@
 #Este primer ejercicio va a hacer de imprimir los primeros 10 números naturales, donde en este caso nos tocará utilizar un "for"
+print ("Bienvenido : Este programa es para que imprima los primeros 10 numeros naturales")
+x=1
+#Vamos a utilizar una condicional while para hacer que me imprima los 10 primeros números siempre y cuando 1 sea menor o igual a 10
+while x<=10:
+    print ("Los 10 primeros números son:",x) 
+x=x+1
+
+
