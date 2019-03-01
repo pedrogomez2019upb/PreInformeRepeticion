cc975cef5b29816ef20d36515068ca894f726e96
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 21:18:43 -0500

    Se corrigió el archivo para que diera los primeros 15 y no los primeros 10

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
-#Ejercicio para imprimir los 10 primeros números pares  
+#Ejercicio para imprimir los 15 primeros números pares  
 #Aún asi vamos a intentar de que la persona pueda ingresar el numero siempre y cuando sea positivo
-x=int(input("Bienvenido! Este programa te permite mostrar los 10 primeros números pares positivos de un número. Por favor ingresa el número positivo: "))
-n=10
+x=int(input("Bienvenido! Este programa te permite mostrar los 15 primeros números pares positivos de un número. Por favor ingresa el número positivo: "))
+n=15
 if x < 0:
     print ("El número es negativo o es 0. Muchas gracias por utilizar el programa")
 else:
