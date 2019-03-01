fdece5aa7ef5ab0c43bc25c2845cd82d6bd13ec3
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 21:27:38 -0500

    Se cambió la programación para poder utilizar la función for y variar un poco los ejercicios.

--- main3.py
+++ main3.py
@@ -1,5 +1,7 @@
 #Algoritmo que imprima los 15 primeros números impares negativos (iniciando en -1)
-#Para este ejercicio nos toca declarar una variable x=-1 , para que el programa inicie desde ese valor
-x=-1
-#Vamos a utilizar otra vez while para ponerle una condicional
-#Lamentablemente no podemos hacer que el usuario ingrese el valor que quiera puesto a que el ejercico exije que se empiece desde -1
+#Para variar un poco vamos a declarar un inicio y final para poder utilizar la función for
+start=-1
+finish=-32
+print ("Bienvenido! A continuación se va a imprimir los 15 primeros números impares negativos.")
+for x in range(start, end -1)
+#Si el número es divisible por cualquier número y 2 no va a ser primo
