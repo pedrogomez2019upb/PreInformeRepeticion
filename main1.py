62ef073359dc82f7d99b52a2ea6e0b71c57be6d7
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 20:55:45 -0500

    Se hizo la modificacion correcta para que funcione con for ya declarandole los primeros 10 numeros naturales

--- main1.py
+++ main1.py
@@ -1,8 +1,12 @@
 #Este primer ejercicio va a hacer de imprimir los primeros 10 números naturales, donde en este caso nos tocará utilizar un "for"
 print ("Bienvenido : Este programa es para que imprima los primeros 10 numeros naturales")
 #Anteriormente vimos que while en este caso no es ideal, entonces para esto vamos a intentar de utilizar la función for para ver que pasaría
-#En este caso utilizaremos count = 0 para decir que cuente hasta el número 10
-count = 0
+#Se analizó el caso y la manera más practica es utilizar for para imprimir los 10 primeros números
+#Para ello pondremos los 10 primeros números naturales
+naturales = [1,2,3,4,5,6,7,8,9,10]
+for naturales in naturales:
+    print("Los 10 primeros números naturales son: {}".format (naturales))
 
+#Desarrollado por Pedro Gómez / ID : 000396221
 
 
