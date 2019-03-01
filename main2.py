86258772c201227323155611189aa4d36d38bcd9
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 21:16:15 -0500

    Finalizamos el programa utilizando la función while y añadiendole la posibilidad del que usuario pueda agregar el valor que quiera.

--- main2.py
+++ main2.py
@@ -1,8 +1,13 @@
 #Ejercicio para imprimir los 10 primeros números pares  
 #Aún asi vamos a intentar de que la persona pueda ingresar el numero siempre y cuando sea positivo
 x=int(input("Bienvenido! Este programa te permite mostrar los 10 primeros números pares positivos de un número. Por favor ingresa el número positivo: "))
+n=10
 if x < 0:
     print ("El número es negativo o es 0. Muchas gracias por utilizar el programa")
-    else:
-        
-        while 
+else:
+        while(x<=(n)):
+            y=x*2
+            print("Los 10 primeros valroes positivos son: {}".format(y))
+            x=x+1
+#Desarrollado por Pedro Gómez / ID : 000396221
+
