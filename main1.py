aa55d295f0a327af7057192071f41d0a1797ffa6
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 20:52:14 -0500

    Vemos un cambio en la programación , pero vamos a intentar de utilizar el comando for aunque while nos funcione

--- main1.py
+++ main1.py
@@ -1,9 +1,8 @@
 #Este primer ejercicio va a hacer de imprimir los primeros 10 números naturales, donde en este caso nos tocará utilizar un "for"
 print ("Bienvenido : Este programa es para que imprima los primeros 10 numeros naturales")
-x=1
-#Vamos a utilizar una condicional while para hacer que me imprima los 10 primeros números siempre y cuando 1 sea menor o igual a 10
-while x<=10:
-    print ("Los 10 primeros números son:",x) 
-x=x+1
+#Anteriormente vimos que while en este caso no es ideal, entonces para esto vamos a intentar de utilizar la función for para ver que pasaría
+#En este caso utilizaremos count = 0 para decir que cuente hasta el número 10
+count = 0
+
 
 
