1566602dd8a9302e42e6f51974d036e30d4cdb80
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 21:22:34 -0500

    Se actualiza el tercer archivo para empezar su desarrollo.

---
+++ main3.py
@@ -0,0 +1,5 @@
+#Algoritmo que imprima los 15 primeros números impares negativos (iniciando en -1)
+#Para este ejercicio nos toca declarar una variable x=-1 , para que el programa inicie desde ese valor
+x=-1
+#Vamos a utilizar otra vez while para ponerle una condicional
+#Lamentablemente no podemos hacer que el usuario ingrese el valor que quiera puesto a que el ejercico exije que se empiece desde -1
