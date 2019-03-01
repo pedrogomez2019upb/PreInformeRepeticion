6fda05ffe3acb66ac2dc6f0616a9a846f5a4deb1
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-02-28 21:07:31 -0500

    El programa no finalizado se sube como proceso de desarrollo de este mismo, con el fin de demostrar el progreso del trabajo.

---
+++ main2.py
@@ -0,0 +1,8 @@
+#Ejercicio para imprimir los 10 primeros números pares  
+#Aún asi vamos a intentar de que la persona pueda ingresar el numero siempre y cuando sea positivo
+x=int(input("Bienvenido! Este programa te permite mostrar los 10 primeros números pares positivos de un número. Por favor ingresa el número positivo: "))
+if x < 0:
+    print ("El número es negativo o es 0. Muchas gracias por utilizar el programa")
+    else:
+        
+        while 
